0ccc3d24ffec16e2864de3b673754863fc1955b8
zelebez6969 <35007597+zelebez6969@users.noreply.github.com>
2018-05-29 08:31:53 +0800

    kicker line

---
+++ bot.py
@@ -0,0 +1,106 @@
+from linepy import *
+#------------------------------------------------------------------------------------------------------------#
+client = LINE()
+client.log("Auth Token : " + str(client.authToken))
+#------------------------------------------------------------------------------------------------------------#
+oepoll = OEPoll(client)
+#------------------------------------------------------------------------------------------------------------#
+MySelf = client.getProfile()
+JoinedGroups = client.getGroupIdsJoined()
+print("My MID : " + MySelf.mid)
+#------------------------------------------------------------------------------------------------------------#
+targets = []
+#------------------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------------------#
+def NOTIFIED_INVITE_INTO_GROUP(op):
+    try:
+        if op.param1 not in JoinedGroups:
+                client.acceptGroupInvitation(op.param1)
+                JoinedGroups.append(op.param1)
+                group = client.getGroup(op.param1)
+                for contact in group.members:
+                    targets.append(contact.mid)
+                if targets == []:
+                    client.leaveGroup(op.param1)
+                    JoinedGroups.remove(op.param1)
+                else:
+                        try:
+                            kick = [targets]
+                            klist = random.choice(kick)
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            client.kickoutFromGroup(op.param1, [klist])
+                            print(op.param1, [contact.mid])
+                        except:
+                            group.name = "DI SITA"
+                            client.updateGroup(group)
+                            client.leaveGroup(op.param1)
+                            JoinedGroups.remove(op.param1)
+    except Exception as e:
+        print(e)
+        print("\n\nNOTIFIED_INVITE_INTO_GROUP\n\n")
+        return
+#------------------------------------------------------------------------------------------------------------#    
+#------------------------------------------------------------------------------------------------------------#
+def KICKOUT_FROM_GROUP(op):
+    try:
+        targets.remove(op.param3)
+    except Exception as e:
+        print(e)
+        print("\n\nKICKOUT_FROM_GROUP\n\n")
+        return
+#------------------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------------------#
+oepoll.addOpInterruptWithDict({
+    OpType.NOTIFIED_INVITE_INTO_GROUP: NOTIFIED_INVITE_INTO_GROUP,
+    OpType.KICKOUT_FROM_GROUP: KICKOUT_FROM_GROUP
+})
+#------------------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------------------#
+while True:
+    oepoll.trace()
